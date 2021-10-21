85ed593362e96363f2ea0824be6729e20e692de6
Warren MacEvoy <wmaceevoy@gmail.com>
2021-10-21 13:20:11 -0600

    fixed!

--- jsonrpc.py
+++ jsonrpc.py
@@ -2,31 +2,35 @@
 
 # pip install requests
 
-
-# Curl ex:
-# 
-# curl -v --user-agent "AuthServiceProxy/0.1" --data-binary '{"version": "1.1", "method": "gettransaction", "params": ["1e519e12a210c9ef458553d05850ffd6441ed354303d623aa0dbcb5c8e29ee8d"], "id": 1}' -H 'context-type: application/json;' -H "Authorization: Basic cnBjdXNlcjplZDJkZWI3MzgxNzUzYWU5NzA4ZWNmNmY5NWM1YmU3NTRjN2M5YjNlODY3ZDVlZDExOTQ3NmM2ZWVjNGY2NTFl" http://127.0.0.1:18332/
-
-
 import requests
 import json
 import configparser
+import pathlib
+from pathlib import Path
 
-def config(file):
+def getConfig(file=None):
+    if (file == None):
+        file = str(pathlib.Path.home() / ".bitcoin" / "bitcoin.conf")
+
     fd = open(file,"r")
     data = fd.read()
     fd.close()
 
-    sectionData=f"""
-    [section]
+    config=configparser.ConfigParser()
+    config.read_string(f"""
+    [default]
     {data}
-    """
+    """)
+    
+    return config['default']
 
-    configparser.ConfigParser()
+config=get_config()
 
-user="rpcuser"
-password="edbb2deb7381753ae9708ecf6f95c5be754c7c9b3e867d5ed119476c6eec4f651e"
-port=18332
+testnet=int(config.get('testnet',0))
+user=str(config.get('rpcuser',''))
+password=str(config.get('rpcpassword',''))
+port=int(config.get('port',8332 if not testnet else 18332))
+
 url = f"http://localhost:{port}/"
 
 payload = {
@@ -35,8 +39,6 @@ payload = {
     "params": ["1e519e12a210c9ef458553d05850ffd6441ed354303d623aa0dbcb5c8e29ee8d"],
     "id": 0,
 }
-session = requests.Session()
-session.auth = (user, password)
 
-response = session.post(url, json=payload)
-print(response)
+response = requests.post(url, json=payload, auth=(user,password))
+print(response.json())
